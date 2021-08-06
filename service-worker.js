a98dd38b7c937671ab9cf83b399cfd9e6694ca0e
kar <hi@kar.gs>
2021-08-06 10:14:40 +0200

    publish landing: b6b488b

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.df5b89aabf5b1cac9582eb5d36f60c01.js"
+  "precache-manifest.6e741285c9432c94c38f50489c23d966.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "landing"});
