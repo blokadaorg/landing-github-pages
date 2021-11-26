040a19b90e37d0a08450583e9470bba2471ba1ab
kar <hi@kar.gs>
2021-11-26 12:16:56 +0100

    publish landing: d60ca69

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.5bbeb60c84cb21eaa59962119cdb68f0.js"
+  "precache-manifest.bb2d55dca465af01754442f1003f4323.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "landing"});
